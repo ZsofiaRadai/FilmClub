cd68d163326f53be223e97b9a616155bcb5d447f
Unknown <zsofiaradai@gmail.com>
2019-01-05 15:24:17 +0100

    add User entity

---
+++ src/main/java/com/mybusiness/filmclub/db/model/User.java
@@ -0,0 +1,40 @@
+package com.mybusiness.filmclub.db.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column(name = "registration")
+    private Date registrationDate;
+
+    @PrePersist
+    protected void onCreate() {
+        registrationDate =  new Date();
+    }
+
+
+}
