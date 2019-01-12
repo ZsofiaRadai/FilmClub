e686fe7dbf4562daa59b0c96367eab1212a7b9b0
Unknown <zsofiaradai@gmail.com>
2019-01-12 09:50:02 +0100

    create new user with hashed password

--- src/main/java/com/mybusiness/filmclub/controller/UserController.java
+++ src/main/java/com/mybusiness/filmclub/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mybusiness.filmclub.controller;
 
 import com.mybusiness.filmclub.db.model.User;
+import com.mybusiness.filmclub.dto.CreateUserRequest;
 import com.mybusiness.filmclub.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,13 @@ public class UserController {
     }
 
     @PostMapping("/createUser")
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public User createUser(@RequestBody CreateUserRequest createUserRequest) {
+        return userService.createUser(createUserRequest);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestHeader String credentials) {
+        User login = userService.login(credentials);
+        return login;
     }
 }
--- src/main/java/com/mybusiness/filmclub/service/UserService.java
+++ src/main/java/com/mybusiness/filmclub/service/UserService.java
@@ -1,7 +1,9 @@
 package com.mybusiness.filmclub.service;
 
+import com.mybusiness.filmclub.configuration.PasswordEncoder;
 import com.mybusiness.filmclub.db.model.User;
 import com.mybusiness.filmclub.db.repository.UserRepository;
+import com.mybusiness.filmclub.dto.CreateUserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +15,22 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> getAll() {
         return userRepository.findAll();
     }
 
-    public User createUser(User user) {
+    public User createUser(CreateUserRequest createUserRequest) {
+        User user = new User();
+        user.setPassword(passwordEncoder.BCyptPasswordEncoder().encode(createUserRequest.getPassword()));
+        user.setEmail(createUserRequest.getEmail());
+        user.setUsername(createUserRequest.getUsername());
         return userRepository.save(user);
+    }
+
+    public User login(String credentials) {
+        return userRepository.findByEmail(credentials);
     }
 }
